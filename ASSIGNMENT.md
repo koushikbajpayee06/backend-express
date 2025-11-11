bb0e4c37ea7c3bc4f3b000233963d600776ae28e
Koushik <Koushikbajpayee06@gmail.com>
2025-11-11 12:27:17 +0530

    why we should not use app.use()

---
+++ ASSIGNMENT.md
@@ -0,0 +1,19 @@
+## EP-03: Creating our Express Server
+- Install Express server
+- What is dependencies?
+- What is package.json, package-lock.json, node_modules?
+- What is the difference between package.json VS package-lock.json?
+- What is ^ , ~ ?
+- What is the use of "-g" while npm install
+- Difference between ~ vs ^ ?
+- create a server.
+- Listen to port 7777
+- Make request handlers for /test, /hello?
+- Install nodemon and update scripts inside package.json.
+
+## EP-04: Routing and Request Handlers
+- Play with routes and route extensions ex, /, /hello, /hello/2, /text, /text/abc/pqr
+- Order of the routes matter a lot
+- Install postman app and make a workspace/collection > test API call
+- app.use vs app.get
+- 
--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.use("/user",(req,res)=>{
+    res.send("HAHAHHAHAHAHAHHAHA");
+})
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Koushik", lastName:"Bajpayee"});
+});
 
+app.post("/user",(req,res)=>{
+    console.log("Save data to database");
+    res.send("Data Successfully saved in database");
+});
+
+app.delete('/user',(req,res)=>{
+    console.log("data deleted");
+    res.send("data deleted successfully")
+})
 app.use("/test",(req,res)=>{
     res.send("Hello from the server...");
 });
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello! Hello! Hello!");
-});
-
-app.use("/",(req,res)=>{
-    res.send("Namaste Koushik");
-});
 
 app.listen(7777,()=>{
     console.log("server is succesfull listining on port 7777");
