77ee0f075d4d1b23dda015f49daa493ae38cb246
Koushik <Koushikbajpayee06@gmail.com>
2025-11-28 22:37:31 +0530

    multiple route handlers

--- src/App.js
+++ src/App.js
@@ -2,10 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params)
-    res.send({firstName:"Koushik", lastName:"Bajpayee"});
-});
+app.use(
+    '/user',
+    (req, res, next)=>{
+    next();
+    console.log("request handler-1")
+    // res.send("Response-1!! ");
+},
+    (req, res, next)=>{
+        console.log("Route Handler-2");
+        // res.send("Response-2!!");
+        next()
+    }
+,
+    (req, res, next)=>{
+        console.log("Route Handler-3");
+        // res.send("Response-3!!");
+        next()
+    }
+,
+    (req, res, next)=>{
+        console.log("Route Handler-4");
+        // res.send("Response-4!!");
+        next()
+    }
+);
 
 app.listen(7777,()=>{
     console.log("server is succesfull listining on port 7777");
