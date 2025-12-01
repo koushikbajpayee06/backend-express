5f5dc0d4c3a7f3f4267aeb4238c1891ed09a9091
Koushik <Koushikbajpayee06@gmail.com>
2025-12-01 10:21:56 +0530

    application connected with cluster through mongoose

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 const express = require('express');
-
+require('./config/database');
 const app = express();
-
-app.use('/',(err, req, res,next)=>{
-    if(err){
-        res.status(500).send("Somthing went wrong")
-    }
-})
-
-app.get("/getUserData",(req,res)=>{
-    throw new Error(" koushik");
-    res.send("User Data Send")
-    
-});
-
-app.use('/',(err, req, res,next)=>{
-    if(err){
-        res.status(500).send("Somthing went wrong-2")
-    }
-})
-
 
 app.listen(7777,()=>{
     console.log("server is succesfull listining on port 7777");
---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+
+const connectDB = async () =>{
+    await mongoose.connect(
+        "mongodb+srv://namastedev:fsmtGY1IfU6h4Ypp@namastenode.uwpmhgp.mongodb.net/revDevTinder"
+    );
+}
+
+connectDB().then(()=>{
+    console.log("Database connection established...")
+}).catch(err=>{
+    console.error("Database cant be connected")
+})
