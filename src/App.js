2c075feb300779acc3abe3b5996a4551fb631a0e
Koushik <Koushikbajpayee06@gmail.com>
2025-12-15 19:44:03 +0530

    feat: configure CORS with credentials support for auth cookies

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ const requestRouter = require('./routes/requests');
 const userRouter = require('./routes/user');
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials: true,
+}));
 app.use('/', authRouter);
 app.use('/', profileRouter);
 app.use('/', requestRouter);
