d9725d5951e75902231f0930d9aec1d7ded8ca05
Koushik <Koushikbajpayee06@gmail.com>
2025-12-20 09:37:30 +0530

    some changes in signup api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -18,8 +18,14 @@ authRouter.post("/signup", async (req, res) => {
       emailId,
       password:passwordHash,
     });
-    await user.save();
-    res.send("User added successfully");
+    const saveUser = await user.save();
+    const token = await saveUser.getJWT();
+      
+    res.cookie("token",token,{
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+    
+    res.json({message:"User Added successfully", data: saveUser});
   } catch (err) {
     res.status(400).send("ERROR  :" + err.message);
   }
